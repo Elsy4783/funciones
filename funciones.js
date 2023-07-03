fd27c0ab0700a35c4c38ca67dd5c6d610903c6b5
Elsy4783 <130095657+Elsy4783@users.noreply.github.com>
2023-07-02 23:01:41 -0300

    First commit

---
+++ funciones.js
@@ -0,0 +1,9 @@
+//PAR O IMPAR//
+
+let num;
+num = prompt("Ingrese un número y elprograma le dirá si es Par o Impar");
+if (num % 2 == 0) {
+  document.write("El número  " + num + " es Par");
+} else {
+  document.write("El número  " + num + " es Impar");
+}
