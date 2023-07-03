d8da7d9358b4f895527cf520bfd39e32473c5085
Elsy4783 <130095657+Elsy4783@users.noreply.github.com>
2023-07-02 23:51:09 -0300

    funcion mayor o menor

--- funciones.js
+++ funciones.js
@@ -1,9 +1,27 @@
-//PAR O IMPAR//
+// 1. PAR O IMPAR//
 
-let num;
-num = prompt("Ingrese un número y elprograma le dirá si es Par o Impar");
-if (num % 2 == 0) {
-  document.write("El número  " + num + " es Par");
-} else {
-  document.write("El número  " + num + " es Impar");
+function parImpar(num) {
+  var num;
+  num = prompt("Ingrese un número y el programa le dirá si es Par o Impar");
+  if (num % 2 == 0) {
+    console.log("El número  " + num + " es Par");
+  } else {
+    document.write("El número  " + num + " es Impar");
+  }
 }
+
+parImpar();
+
+// 2. MAYOR O MENOR//
+
+function mayorOmenor(numero1, numero2) {
+  if (numero1 > numero2) {
+    console.log(numero1 + " es Mayor que " + numero2);
+  } else if (numero1 < numero2) {
+    console.log(numero1 + " es Menor que " + numero2);
+  } else {
+    console.log("Los números ingresados con Iguales");
+  }
+}
+
+mayorOmenor(5, 7);
